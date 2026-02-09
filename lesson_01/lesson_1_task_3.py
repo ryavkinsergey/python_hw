3c2b3a49c45d6ad5c4ad286fb70ce57fd9823eea
ryavkinsergey <bloodua@mail.ru>
2026-02-09 15:40:14 +0500

    lesson_03

--- lesson_01/lesson_1_task_3.py
+++
@@ -1,3 +0,0 @@
-first_name = input("Имя:")
-last_name = input("Фамилия:")
-print(f"Вас зовут:{first_name} {last_name}")
--- lesson_02/lesson_2_task_2.py
+++
@@ -1,7 +0,0 @@
-def is_year_leap(years):
-    return years % 4 == 0
-
-
-year = 2024
-result = is_year_leap(year)
-print(f"год {year}: {result}")
